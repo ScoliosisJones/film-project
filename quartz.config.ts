5603eaaf338f6a0632514e6fe7e9c1913c020955
Jeremy Smith <jeremymichaelsmith1003@gmail.com>
2026-02-26 22:29:47 -0800

    Fix baseUrl for GitHub Pages

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "ScoliosisJones.github.io/film-project",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
